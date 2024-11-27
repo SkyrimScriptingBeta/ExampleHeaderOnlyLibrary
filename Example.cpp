a1d4c6deed9258fab4e9e7ba396f8411b7b04dc8
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-26 18:36:22 -0800

    Format the things!

--- Example.cpp
+++ Example.cpp
@@ -1,9 +1,10 @@
-#include "ExampleHeaderOnlyLibrary.h"
-
 #include <iostream>
 
+#include "ExampleHeaderOnlyLibrary.h"
+
+
 int main() {
-  std::cout << "Example header-only library value: "
-            << get_example_header_only_library_value() << std::endl;
-  return 0;
+    std::cout << "Example header-only library value: " << get_example_header_only_library_value()
+              << std::endl;
+    return 0;
 }
